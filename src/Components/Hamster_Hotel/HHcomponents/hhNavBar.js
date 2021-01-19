1394ee142989b880cfc07741d20623aa4ae8069f
Matthew-J-Mendoza <mattjm112999@gmail.com>
2021-01-19 14:46:10 -0800

    dfasdfs

--- src/Components/Hamster_Hotel/HHcomponents/hhNavBar.js
+++ src/Components/Hamster_Hotel/HHcomponents/hhNavBar.js
@@ -1,24 +1,40 @@
 
-import { Navbar, Nav, NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown,Container,Row,Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../HHstyle.css'
 
 export const HHnav = () =>{
     return(
             <Navbar bg="dark" variant="dark" expand="lg" className="navlayout">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Container>
+        <Row>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+            <Col>
+            <img src="../images/pool.png" />
+            </Col>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+            <Col>
+            <Nav.Link href="#link">Link</Nav.Link>
+            </Col>
+        </Row>
+    </Container>
+      
     </Nav>
   </Navbar.Collapse>
 </Navbar>

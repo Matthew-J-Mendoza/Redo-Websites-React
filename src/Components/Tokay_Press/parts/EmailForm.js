e2d703fca6f3ac45b13f5f6205b6b0ec3d6589a1
Matthew-J-Mendoza <mattjm112999@gmail.com>
2021-03-05 15:12:37 -0800

    Made TP forms work in React

--- src/Components/Tokay_Press/parts/EmailForm.js
+++ src/Components/Tokay_Press/parts/EmailForm.js
@@ -7,25 +7,46 @@ import React from 'react'
 
 
 export class EmailForms extends React.Component{
-    
-    Submit=()=>{
+    constructor(props){
+        super(props)
+        this.state={
+            messeageSent: false,
+            EamilValue: '',
+            NameValue: '',
+            MessageValue: '',
+            Name: React.createRef(),
+            Email: React.createRef(),
+            Message: React.createRef(),
+        }
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleMessageChange = this.handleMessageChange.bind(this);
+      }
+      handleNameChange(event) {
+          this.setState({NameValue: event.target.value});
+          console.log(this.state.NameValue)
+        }
+        handleEmailChange(event) {
+            this.setState({EamilValue: event.target.value});
+            console.log(this.state.EamilValue)
+        }
+        handleMessageChange(event) {
+            this.setState({MessageValue: event.target.value});
+            console.log(this.state.MessageValue.length+1)
+        }
         
-        let messageBtn = document.getElementById("messageBtn");
-let inputName = document.getElementById('NameInput');
-let inputEmail = document.getElementById('formBasicEmail');
-
-
-if (inputName.value !== "" && inputEmail.value !=="" && inputEmail.value!== ""){
-    messageBtn.classList.add('messageSent');
-    messageBtn.innerText = "MESSAGE SENT";
-    alert("Thank you, "+ inputName.value+"! We will contact you soon at the email you provided: ("+inputEmail.value+").");
-  
-    messageBtn.innerHTML = '<b>MESSAGE SENT! </b><i class="far fa-paper-plane pl-1"></i>';
- } else{
-    alert("Uh oh! Please fill out all fields before sending.");
- }
-    }
-
+        Submit=()=>{
+            if (this.state.Name.current.value != "" && this.state.Email.current.value !="" && this.state.Message.current.value!= ""){
+                alert("Thank you, "+ this.state.Name.current.value+"! We will contact you soon at the email you provided: ("+this.state.Email.current.value+").")
+                this.setState({
+                    messeageSent: true,
+                    EamilValue: "",
+                    NameValue: "",
+                    MessageValue: "",
+                })
+            }
+            console.log(this.state.Name.current.value)
+        }
 
     render(){
         return (
@@ -33,27 +54,29 @@ if (inputName.value !== "" && inputEmail.value !=="" && inputEmail.value!== ""){
                 <Row>
                     <Form.Group required controlId="NameInput" className="col-12 col-md-6">
                         <Form.Label className="sr-only">Your Full Name</Form.Label>
-                        <Form.Control type="type" placeholder="Your Full Name" />
+                        <Form.Control type="type" placeholder="Your Full Name" value={this.state.NameValue} onChange={this.handleNameChange} ref={this.state.Name} />
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
                     <Form.Group required controlId="formBasicEmail" className="col-12 col-md-6">
                         <Form.Label className="sr-only">Your Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Control type="email" placeholder="name@example.com" value={this.state.EamilValue} onChange={this.handleEmailChange}  ref={this.state.Email}/>
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
                 </Row>
                     <Form.Group required controlId="ControlTextarea1">
                         <Form.Label className="sr-only">Tell us more about your project!</Form.Label>
-                        <Form.Control as="textarea" rows={7} placeholder="Tell us more about your project!" />
+                        <Form.Control as="textarea" rows={7} placeholder="Tell us more about your project!" value={this.state.MessageValue} onChange={this.handleMessageChange} ref={this.state.Message}/>
                     </Form.Group>
                     <Row>
                         <Col className="d-flex justify-content-end">
-                        <button onClick={this.Submit} type="button" id="messageBtn" className="btn btn-brightblue TP-btn"><i className="far fa-envelope pr-1"></i><b>SEND MESSAGE</b></button>
+                        <button onClick={this.Submit} type="button" id="messageBtn" className={`btn btn-brightblue TP-btn ${this.state.messeageSent === true? "messageSent" : ""}`}><b>{this.state.messeageSent === false ? 'SEND MESSAGE' : "MESSAGE SENT!"}</b><i className={`far fa-${this.state.messeageSent === true ? "paper-plane" : "envelope"} pl-1`}></i></button>
+                        
                         </Col>
-                    </Row>
+                    </Row>                           
             </Form>
         )
     }
+    // {this.messeageSent === true ? <div> <b>MESSAGE SENT! </b><i class="far fa-paper-plane pl-1"></i></div>: <div><i className="far fa-envelope pr-1"></i><b>SEND MESSAGE</b></div>} 
 }
